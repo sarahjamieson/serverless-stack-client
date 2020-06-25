e674a13ee84c19a10c79013492532dc5707c3272
sarahjamieson <sarahjamieson@github.com>
2020-06-25 15:02:07 +0100

    setting up our react app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "notes-app-uploads-65"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://5q1nquaqka.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_F1kJDb2zT",
+    APP_CLIENT_ID: "14asvj5qej0klmuf7p53la2h9n",
+    IDENTITY_POOL_ID: "eu-west-2:3a84cf74-7ef9-4516-86f3-72812abee719"
+  }
+};
